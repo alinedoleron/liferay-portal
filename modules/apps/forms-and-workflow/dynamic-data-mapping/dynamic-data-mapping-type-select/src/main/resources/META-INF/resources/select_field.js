79be8f2431dc357499f4bef489f0536603a658c0
Pedro Queiroz <pedro.queiroz@liferay.com>
2018-05-02 11:28:59 -0300

    LPS-76558 Auto SF

--- modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-select/src/main/resources/META-INF/resources/select_field.js
+++ modules/apps/forms-and-workflow/dynamic-data-mapping/dynamic-data-mapping-type-select/src/main/resources/META-INF/resources/select_field.js
@@ -19,9 +19,9 @@ AUI.add(
 
 		var CSS_SELECT_BADGE_ITEM_CLOSE = A.getClassName('trigger', 'badge', 'item', 'close');
 
-		var CSS_SELECT_OPTION_ITEM = A.getClassName('select', 'option', 'item');
-
 		var CSS_SELECT_DROPDOWN_ITEM = A.getClassName('dropdown', 'item');
+
+		var CSS_SELECT_OPTION_ITEM = A.getClassName('select', 'option', 'item');
 
 		var CSS_SELECT_TRIGGER_ACTION = A.getClassName('select', 'field', 'trigger');
 
@@ -234,28 +234,6 @@ AUI.add(
 						instance.render();
 					},
 
-					_validateValue: function(value) {
-						var instance = this;
-
-						var fixedOptions = instance.get('fixedOptions');
-
-						var options = instance.get('options');
-
-						var fieldOptions = options.concat(fixedOptions);
-
-						var valid = false;
-
-						for (var indexOption in fieldOptions) {
-							for (var indexValue in value) {
-								if (fieldOptions[indexOption].value == value[indexValue]) {
-									valid = true;
-								}
-							}
-						}
-
-						return (value.length == 0) || valid;
-					},
-
 					showErrorMessage: function() {
 						var instance = this;
 
@@ -282,7 +260,7 @@ AUI.add(
 					_afterClickOutside: function(event) {
 						var instance = this;
 
-						if (!instance._preventDocumentClick && instance._isClickingOutSide(event)) {
+						if (!instance._preventDocumentClick && instance._isClickingOutside(event)) {
 							instance.closeList();
 						}
 
@@ -341,10 +319,7 @@ AUI.add(
 						var optionNode = target.ancestor('.' + CSS_SELECT_OPTION_ITEM, true);
 
 						if (instance.get('multiple')) {
-							var optionNode = target.ancestor('.' + CSS_SELECT_DROPDOWN_ITEM, true);
-						} else {
-							var optionNode = target.ancestor('.' + CSS_SELECT_OPTION_ITEM, true);
-
+							optionNode = target.ancestor('.' + CSS_SELECT_DROPDOWN_ITEM, true);
 						}
 
 						if (closeIconNode) {
@@ -429,7 +404,7 @@ AUI.add(
 						return hasOption;
 					},
 
-					_isClickingOutSide: function(event) {
+					_isClickingOutside: function(event) {
 						var instance = this;
 
 						var container = instance.get('container');
@@ -530,6 +505,28 @@ AUI.add(
 						}
 
 						return showSearch;
+					},
+
+					_validateValue: function(value) {
+						var instance = this;
+
+						var fixedOptions = instance.get('fixedOptions');
+
+						var options = instance.get('options');
+
+						var fieldOptions = options.concat(fixedOptions);
+
+						var valid = false;
+
+						for (var indexOption in fieldOptions) {
+							for (var indexValue in value) {
+								if (fieldOptions[indexOption].value == value[indexValue]) {
+									valid = true;
+								}
+							}
+						}
+
+						return (value.length == 0) || valid;
 					}
 				}
 			}
