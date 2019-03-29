25abca91e1de83d7e03070edf3c6ce8226ed4360
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2019-03-29 18:21:17 -0300

    LPS-93069 Add a method to add Field Set on Form Builder

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/metal/js/components/LayoutProvider/LayoutProvider.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/metal/js/components/LayoutProvider/LayoutProvider.es.js
@@ -11,6 +11,7 @@ import handleFieldBlurred from './handlers/fieldBlurredHandler.es';
 import handleFieldDeleted from './handlers/fieldDeletedHandler.es';
 import handleFieldDuplicated from './handlers/fieldDuplicatedHandler.es';
 import handleFieldEdited from './handlers/fieldEditedHandler.es';
+import handleFieldSetAdded from './handlers/fieldSetAddedHandler.es';
 
 /**
  * LayoutProvider listens to your children's events to
@@ -167,6 +168,10 @@ class LayoutProvider extends Component {
 
 	_handleFieldAdded(event) {
 		this.setState(handleFieldAdded(this.props, this.state, event));
+	}
+
+	_handleFieldSetAdded(event) {
+		this.setState(handleFieldSetAdded(this.props, this.state, event));
 	}
 
 	_handleFieldBlurred(event) {
@@ -485,6 +490,7 @@ class LayoutProvider extends Component {
 				fieldDuplicated: this._handleFieldDuplicated.bind(this),
 				fieldEdited: this._handleFieldEdited.bind(this),
 				fieldMoved: this._handleFieldMoved.bind(this),
+				fieldSetAdded: this._handleFieldSetAdded.bind(this),
 				focusedFieldUpdated: this._handleFocusedFieldUpdated,
 				pageAdded: this._handlePageAdded.bind(this),
 				pageDeleted: this._handlePageDeleted.bind(this),
