741df26d201f1cc2d95f56ee7085555aed37a380
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2019-04-16 12:31:37 -0300

    LPS-92863 Keep using type when second operand if different from numeric types

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/RuleEditor/RuleEditor.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/RuleEditor/RuleEditor.es.js
@@ -590,10 +590,16 @@ class RuleEditor extends Component {
 					firstOperandOptions,
 					operands: condition.operands.map(
 						(operand, index) => {
-							if (index === 1 && operand.type !== 'field' && operand.type !== 'user') {
+							if (index === 1 && (operand.type == 'integer' || operand.type == 'double')) {
 								operand = {
 									...operand,
 									type: getFieldProperty(pages, condition.operands[0].value, 'dataType')
+								};
+							}
+							else if (index === 1 && operand.type !== 'field' && operand.type !== 'user') {
+								operand = {
+									...operand,
+									type: getFieldProperty(pages, condition.operands[0].value, 'type')
 								};
 							}
 
