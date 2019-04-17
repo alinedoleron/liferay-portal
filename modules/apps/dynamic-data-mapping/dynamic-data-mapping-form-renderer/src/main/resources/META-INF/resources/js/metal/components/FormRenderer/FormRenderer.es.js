de0a5e6d446f5339123759e0fee929e038be74d3
Bruno Basto <bruno.basto@liferay.com>
2019-04-17 15:06:47 -0300

    LPS-93027 Move renderer files to renderer module

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/metal/components/FormRenderer/FormRenderer.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/resources/META-INF/resources/js/metal/components/FormRenderer/FormRenderer.es.js
@@ -1,11 +1,9 @@
-import '../Page/PageRenderer.es';
-import '../SuccessPage/SuccessPage.es';
+import '../PageRenderer/PageRenderer.es';
 import 'clay-button';
 import Component from 'metal-component';
 import Soy from 'metal-soy';
 import templates from './FormRenderer.soy.js';
 import {Config} from 'metal-state';
-import {pageStructure} from '../../util/config.es';
 
 /**
  * FormRenderer.
