45e0c43a21ce542f4bd854099e28298fe57b29c8
Bruno Basto <bruno.basto@liferay.com>
2019-04-17 15:08:24 -0300

    LPS-93027 Updates references

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/LayoutProvider/util/settings.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/LayoutProvider/util/settings.es.js
@@ -1,4 +1,4 @@
-import {PagesVisitor} from '../../../util/visitors.es';
+import {PagesVisitor} from 'dynamic-data-mapping-form-renderer/js/metal/util/visitors.es';
 
 export const updateSettingsContextProperty = (
 	editingLanguageId,
