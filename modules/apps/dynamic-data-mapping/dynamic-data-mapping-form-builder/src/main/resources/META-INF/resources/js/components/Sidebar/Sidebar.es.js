327342bcdcc11402257826d409f5b1fb6ed02c7b
Bruno Basto <bruno.basto@liferay.com>
2019-05-07 17:51:02 -0300

    LPS-93027 We need portletNamespace for evaluations

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/Sidebar/Sidebar.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/Sidebar/Sidebar.es.js
@@ -824,6 +824,7 @@ class Sidebar extends Component {
 		const {activeTab} = this.state;
 		const {
 			editingLanguageId,
+			portletNamespace,
 			spritemap
 		} = this.props;
 		const {pages, rules} = this.getSettingsFormContext();
@@ -842,6 +843,7 @@ class Sidebar extends Component {
 				events={formEvents}
 				pages={pages}
 				paginationMode="tabbed"
+				portletNamespace={portletNamespace}
 				ref="evaluableForm"
 				rules={rules}
 				spritemap={spritemap}
