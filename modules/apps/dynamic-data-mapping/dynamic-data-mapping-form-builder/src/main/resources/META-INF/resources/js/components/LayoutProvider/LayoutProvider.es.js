9fb24cb675f8f7cfcfb22e443ae5546f738787e2
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2020-05-19 21:43:21 -0300

    LPS-112002 Reorder methods alphabetically

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/LayoutProvider/LayoutProvider.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/LayoutProvider/LayoutProvider.es.js
@@ -433,25 +433,6 @@ class LayoutProvider extends Component {
 		});
 	}
 
-	_handlePagesSwapped({firstIndex, secondIndex}) {
-		const {pages} = this.state;
-
-		const [firstPage, secondPage] = [pages[firstIndex], pages[secondIndex]];
-
-		this.setState({
-			pages: pages.map((page, index) => {
-				if (index === firstIndex) {
-					return secondPage;
-				}
-				else if (index === secondIndex) {
-					return firstPage;
-				}
-
-				return page;
-			}),
-		});
-	}
-
 	_handlePageDeleted(pageIndex) {
 		const {pages} = this.state;
 
@@ -468,6 +449,25 @@ class LayoutProvider extends Component {
 
 		this.setState({
 			pages,
+		});
+	}
+
+	_handlePagesSwapped({firstIndex, secondIndex}) {
+		const {pages} = this.state;
+
+		const [firstPage, secondPage] = [pages[firstIndex], pages[secondIndex]];
+
+		this.setState({
+			pages: pages.map((page, index) => {
+				if (index === firstIndex) {
+					return secondPage;
+				}
+				else if (index === secondIndex) {
+					return firstPage;
+				}
+
+				return page;
+			}),
 		});
 	}
 
