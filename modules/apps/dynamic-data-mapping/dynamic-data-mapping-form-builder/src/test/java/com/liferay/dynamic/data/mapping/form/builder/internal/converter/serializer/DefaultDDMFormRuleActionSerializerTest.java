fcbfe0edd40df0a3098120e4c246f3e960f02e8b
Carolina <carolina.barbosa@liferay.com>
2020-10-09 14:51:03 -0300

    LPS-119818 Add unit tests for rule action serializer

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/test/java/com/liferay/dynamic/data/mapping/form/builder/internal/converter/serializer/DefaultDDMFormRuleActionSerializerTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/test/java/com/liferay/dynamic/data/mapping/form/builder/internal/converter/serializer/DefaultDDMFormRuleActionSerializerTest.java
@@ -29,6 +29,20 @@ import org.mockito.Mock;
 public class DefaultDDMFormRuleActionSerializerTest {
 
 	@Test
+	public void testSerializeWithEmptyTarget() {
+		DefaultDDMFormRuleAction defaultDDMFormRuleAction =
+			new DefaultDDMFormRuleAction("show", StringPool.BLANK);
+
+		DefaultDDMFormRuleActionSerializer defaultDDMFormRuleActionSerializer =
+			new DefaultDDMFormRuleActionSerializer(defaultDDMFormRuleAction);
+
+		String result = defaultDDMFormRuleActionSerializer.serialize(
+			_spiDDMFormRuleSerializerContext);
+
+		Assert.assertNull(result);
+	}
+
+	@Test
 	public void testSerializeSetEnabled() {
 		DefaultDDMFormRuleAction defaultDDMFormRuleAction =
 			new DefaultDDMFormRuleAction("enable", "field0");
