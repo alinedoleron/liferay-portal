9eb151a4e0791e175756cf688f9cbaf4c5976c8b
Carolina Barbosa <carolina.barbosa@liferay.com>
2021-06-23 21:53:33 -0300

    LPS-134607 Set numericInputMask properties into template context

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/java/com/liferay/dynamic/data/mapping/form/renderer/internal/DDMFormFieldTemplateContextFactory.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-renderer/src/main/java/com/liferay/dynamic/data/mapping/form/renderer/internal/DDMFormFieldTemplateContextFactory.java
@@ -496,6 +496,23 @@ public class DDMFormFieldTemplateContextFactory {
 		ddmFormFieldTemplateContext.put("fieldReference", fieldReference);
 	}
 
+	protected void setDDMFormFieldTemplateContextInputMaskProperties(
+		Map<String, Object> ddmFormFieldTemplateContext,
+		Map<String, Object> changedProperties) {
+
+		if (!addProperty(changedProperties, "inputMask")) {
+			return;
+		}
+
+		ddmFormFieldTemplateContext.put(
+			"inputMask",
+			MapUtil.getBoolean(changedProperties, "inputMask", false));
+		ddmFormFieldTemplateContext.put(
+			"inputMaskFormat", changedProperties.get("inputMaskFormat"));
+		ddmFormFieldTemplateContext.put(
+			"numericInputMask", changedProperties.get("numericInputMask"));
+	}
+
 	protected void setDDMFormFieldTemplateContextInstanceId(
 		Map<String, Object> ddmFormFieldTemplateContext, String instanceId) {
 
@@ -875,6 +892,8 @@ public class DDMFormFieldTemplateContextFactory {
 		setDDMFormFieldTemplateContextEvaluable(
 			ddmFormFieldTemplateContext, ddmFormField, changedProperties,
 			ddmFormField.getProperty("evaluable"));
+		setDDMFormFieldTemplateContextInputMaskProperties(
+			ddmFormFieldTemplateContext, changedProperties);
 		setDDMFormFieldTemplateContextLocalizedValue(
 			ddmFormFieldTemplateContext, "requiredErrorMessage",
 			ddmFormField.getRequiredErrorMessage());
