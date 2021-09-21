e6c071478789ab01bea9869c960e4d3bd5a06b18
Aline Éllida D'Oleron Vasconcelos Cantarelli <aline.cantarelli@liferay.com>
2021-09-21 12:16:36 -0300

    LPS-139476 Create tests to guarantee the value is not being overridden by predefined value when the field has been edited

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/test/js/Select/Select.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/test/js/Select/Select.es.js
@@ -542,4 +542,63 @@ describe('Select', () => {
 			'item11',
 		]);
 	});
+
+	it('shows the options value if there are values', async () => {
+		const handleFieldEdited = jest.fn();
+
+		const {container} = render(
+			<SelectWithProvider
+				dataSourceType="manual"
+				multiple={true}
+				onChange={handleFieldEdited}
+				options={createOptions(12)}
+				predefinedValue={['item1', 'item2']}
+				spritemap={spritemap}
+				value={['item3']}
+			/>
+		);
+
+		expect(document.querySelector('span[value="item1"]')).toBeNull();
+		expect(document.querySelector('span[value="item2"]')).toBeNull();
+		expect(document.querySelector('span[value="item3"]')).not.toBeNull();
+	});
+
+	it('shows the predefinedValues if there is no value', async () => {
+		const handleFieldEdited = jest.fn();
+
+		const {container} = render(
+			<SelectWithProvider
+				dataSourceType="manual"
+				multiple={true}
+				onChange={handleFieldEdited}
+				options={createOptions(12)}
+				predefinedValue={['item1', 'item2']}
+				spritemap={spritemap}
+				value={[]}
+			/>
+		);
+
+		expect(document.querySelector('span[value="item1"]')).not.toBeNull();
+		expect(document.querySelector('span[value="item2"]')).not.toBeNull();
+	});
+
+	it('clear all values if the user has edited the field to clear the predefinedValue', async () => {
+		const handleFieldEdited = jest.fn();
+
+		const {container} = render(
+			<SelectWithProvider
+				dataSourceType="manual"
+				localizedValueEdited={{en_US: true}}
+				multiple={true}
+				onChange={handleFieldEdited}
+				options={createOptions(12)}
+				predefinedValue={['item1', 'item2']}
+				spritemap={spritemap}
+				value={[]}
+			/>
+		);
+
+		expect(document.querySelector('span[value="item1"]')).toBeNull();
+		expect(document.querySelector('span[value="item2"]')).toBeNull();
+	});
 });
