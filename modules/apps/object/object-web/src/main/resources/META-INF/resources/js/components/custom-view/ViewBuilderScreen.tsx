370c4fd0d233c9efa2ba15f86d4fb5059c4c903e
Daniel Bonasser <daniel.bonasser@liferay.com>
2022-01-06 18:05:30 -0300

    LPS-144848 Create View Builder Screen

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/custom-view/ViewBuilderScreen.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/custom-view/ViewBuilderScreen.tsx
@@ -12,10 +12,134 @@
  * details.
  */
 
-import React from 'react';
+import React, { useState } from 'react';
+import {ClayButtonWithIcon} from '@clayui/button';
+import {ClayInput} from '@clayui/form';
+import {useModal} from '@clayui/modal';
+import ClayManagementToolbar from '@clayui/management-toolbar';
+import ClayEmptyState from '@clayui/empty-state';
+import ClayButton from '@clayui/button';
+
+import Card from '../Card/Card';
+import ModalAddObjectLayout from '../ModalAddObjectLayout';
+
+import './ViewBuilderScreen.scss';
+import Panel from '../Panel/Panel';
 
 const ViewBuilderScreen = () => {
-	return <h1>View Builder Screen</h1>;
+
+	const [visibleModal, setVisibleModal] = useState(false);
+	const {observer, onClose} = useModal({
+		onClose: () => setVisibleModal(false),
+	});
+
+    const columnList: Array<any> = [
+        {
+            title: "Name"
+        },
+        {
+            title: "Age"
+        },
+        {
+            title: "Description"
+        },
+        {
+            title: "City"
+        },
+        {
+            title: "Email"
+        },
+    ];
+
+	return (
+        <>
+            <Card>
+                <Card.Header title="Columns"/>
+
+                <Card.Body >
+                    <ClayManagementToolbar >
+                        <ClayManagementToolbar.ItemList expand>
+                            <ClayManagementToolbar.Search>
+                            <ClayInput.Group>
+                                <ClayInput.GroupItem>
+                                <ClayInput
+                                    aria-label="Search"
+                                    className="form-control input-group-inset input-group-inset-after"
+                                    defaultValue="Search"
+                                    type="text"
+                                />
+                                <ClayInput.GroupInsetItem after tag="span">
+                                    <ClayButtonWithIcon
+                                    className="navbar-breakpoint-d-none"
+                                    displayType="unstyled"
+                                    onClick={() => {}}
+                                
+                                    symbol="times"
+                                    />
+                                    <ClayButtonWithIcon
+                                    displayType="unstyled"
+                                
+                                    symbol="search"
+                                    type="submit"
+                                    />
+                                </ClayInput.GroupInsetItem>
+                                </ClayInput.GroupItem>
+                            </ClayInput.Group>
+                            </ClayManagementToolbar.Search>
+
+                            <ClayManagementToolbar.Item>
+                            <ClayButtonWithIcon
+                                className="nav-btn nav-btn-monospaced"
+                                symbol="plus"
+                                onClick={() => setVisibleModal(true)}
+                            />
+                            </ClayManagementToolbar.Item>
+                        </ClayManagementToolbar.ItemList>
+                    </ClayManagementToolbar>
+
+                    {columnList.length > 0 ? (
+                        columnList.map(element => {
+                            return (
+                                <div>
+                                    <Panel>
+                                        <Panel.SimpleBody 
+                                            title={element.title}
+                                            contentRight={
+                                                <ClayButtonWithIcon displayType="unstyled" symbol="times"/>
+                                            }
+                                        >
+                                        </Panel.SimpleBody>
+                                    </Panel>
+                                </div>
+                            )
+                        })
+                    ) : (
+                        <div className="object-web__custom-view-view-builder--empty-space">
+                            <ClayEmptyState 
+                                title='No coluns added yet.' 
+                                description='Add Columns to start creating a View'
+                            >
+                                <ClayButton 
+                                    displayType="secondary"
+                                    onClick={() => setVisibleModal(true)}
+                                >
+                                    {"Add Column"}
+                                </ClayButton>
+                            </ClayEmptyState>
+                        </div>
+                    )}
+
+                </Card.Body>
+            </Card>
+            
+            {visibleModal && (
+                    <ModalDeAlininha
+                        observer={observer}
+                        onClose={onClose}
+                    />
+                )} 
+        </>
+    )
 };
 
 export default ViewBuilderScreen;
