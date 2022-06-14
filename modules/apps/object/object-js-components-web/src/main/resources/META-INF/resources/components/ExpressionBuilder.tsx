9984054ff4f7d228a57fc516fa2486f88fbd5d28
Aline Cantarelli <aline.cantarelli@liferay.com>
2022-06-14 16:03:23 -0300

    LPS-155772 Validate expression builder modal
    Co-authored-by: Carolina Barbosa <carolina.barbosa@liferay.com>

--- modules/apps/object/object-js-components-web/src/main/resources/META-INF/resources/components/ExpressionBuilder.tsx
+++ modules/apps/object/object-js-components-web/src/main/resources/META-INF/resources/components/ExpressionBuilder.tsx
@@ -15,6 +15,7 @@
 import ClayButton, {ClayButtonWithIcon} from '@clayui/button';
 import {ClayInput} from '@clayui/form';
 import ClayModal, {useModal} from '@clayui/modal';
+import {createResourceURL, fetch} from 'frontend-js-web';
 import React, {useEffect, useRef, useState} from 'react';
 
 import CodeEditor, {SidebarCategory} from './CodeEditor/index';
@@ -80,14 +81,22 @@ export function ExpressionBuilder({
 export function ExpressionBuilderModal({sidebarElements}: IModalProps) {
 	const {observer, onOpenChange} = useModal();
 	const editorRef = useRef<CodeMirror.Editor>(null);
-	const [{error, onSave, source}, setState] = useState<{
+	const [
+		{error, onSave, source, validateExpressionBuilderContentURL},
+		setState,
+	] = useState<{
 		error?: string;
 		onSave?: Callback;
 		source?: string;
+		validateExpressionBuilderContentURL?: string;
 	}>({});
 
 	useEffect(() => {
-		const openModal = (params: {onSave: Callback; source: string}) => {
+		const openModal = (params: {
+			onSave: Callback;
+			source: string;
+			validateExpressionBuilderContentURL: string;
+		}) => {
 			setState(params);
 		};
 
@@ -109,17 +118,47 @@ export function ExpressionBuilderModal({sidebarElements}: IModalProps) {
 		onOpenChange(false);
 	};
 
-	const handleSave = () => {
+	const handleSave = async () => {
 		const source = editorRef.current?.getValue();
 
-		if (source?.trim()) {
+		if (!source?.trim()) {
+			setState((state) => ({
+				...state,
+				error: Liferay.Language.get('required'),
+			}));
+
+			return;
+		}
+
+		let message = '';
+		let valid = true;
+
+		if (
+			Liferay.FeatureFlags['LPS-152735'] &&
+			validateExpressionBuilderContentURL
+		) {
+			const response = await fetch(
+				createResourceURL(validateExpressionBuilderContentURL, {
+					expression: source,
+				}).href
+			);
+
+			const responseJson = (await response.json()) as {
+				message?: string;
+				valid: boolean;
+			};
+
+			message = responseJson.message || message;
+			valid = responseJson.valid;
+		}
+		if (valid) {
 			onSave?.(source);
 			closeModal();
 		}
 		else {
 			setState((state) => ({
 				...state,
-				error: Liferay.Language.get('required'),
+				error: message,
 			}));
 		}
 	};
@@ -151,7 +190,7 @@ export function ExpressionBuilderModal({sidebarElements}: IModalProps) {
 						</ClayButton>
 
 						<ClayButton onClick={handleSave}>
-							{Liferay.Language.get('save')}
+							{Liferay.Language.get('done')}
 						</ClayButton>
 					</ClayButton.Group>
 				}
