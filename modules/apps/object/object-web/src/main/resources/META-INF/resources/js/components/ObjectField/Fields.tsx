5a3faf1edae81d965c96512a39c085263ee711f2
Daniel Bonasser <danielbonasser@gmail.com>
2023-06-16 19:01:26 -0300

    LPS-186735-2 Convert fields navigation tab to React

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectField/Fields.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ObjectField/Fields.tsx
@@ -126,7 +126,7 @@ export default function Fields({
 
 		portletId:
 			'com_liferay_object_web_internal_object_definitions_portlet_ObjectDefinitionsPortlet',
-		style,
+		style: 'fluid' as 'fluid',
 		views: [
 			{
 				contentRenderer: 'table',
